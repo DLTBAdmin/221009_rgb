4c30fcda36e54996a648d4ec15e54a6c747d8bb3
DLTBAdmin <107233881+DLTBAdmin@users.noreply.github.com>
2023-07-05 09:39:20 +0800

    Delete mybatis-plus directory

--- mybatis-plus/src/main/java/com/dalao/mybatisplus/mapper/ProductMapper.java
+++
@@ -1,8 +0,0 @@
-package com.dalao.mybatisplus.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.dalao.mybatisplus.entity.Product;
-
-public interface ProductMapper extends BaseMapper<Product> {
-
-}
